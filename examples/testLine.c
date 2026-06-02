ba196532864aaadeb86ec99dec325edf2ce33030
frank <259405825+frandev342@users.noreply.github.com>
2026-06-02 09:21:48 -0500

    Añadiendo lineas de codigo para mover a la tortuga a un lugar adecuadro para comenzar

--- examples/testLine.c
+++ examples/testLine.c
@@ -1,49 +1,47 @@
 #include "../turtlec.h"
 
-void fractalTree ( Turtle * turtle , float length , int depth) {
-	if( depth == 0 || length < 5)
-		return ;
-	turtleForward ( turtle , length ) ;
+void fractalTree(Turtle *turtle, float length, int depth) {
+  if (depth == 0 || length < 5)
+    return;
+  turtleForward(turtle, length);
 
-	turtleLeft ( turtle , 30) ;
-	fractalTree ( turtle , length * 0.8 , depth - 1) ;
+  turtleLeft(turtle, 30);
+  fractalTree(turtle, length * 0.8, depth - 1);
 
-	turtleRight ( turtle , 60) ;
-	fractalTree ( turtle , length * 0.8 , depth - 1) ;
+  turtleRight(turtle, 60);
+  fractalTree(turtle, length * 0.8, depth - 1);
 
-	turtleLeft ( turtle , 30) ;
-	turtleBackward ( turtle , length ) ;
+  turtleLeft(turtle, 30);
+  turtleBackward(turtle, length);
 }
 
-void levy ( Turtle * turtle , float length , int depth ) {
-	if( depth == 0) {
-		turtleForward ( turtle , length ) ;
-		return ;
-	}
-	turtleLeft ( turtle , 45) ;
-	levy ( turtle , length / sqrt (2) , depth - 1) ;
+void levy(Turtle *turtle, float length, int depth) {
+  if (depth == 0) {
+    turtleForward(turtle, length);
+    return;
+  }
+  turtleLeft(turtle, 45);
+  levy(turtle, length / sqrt(2), depth - 1);
 
-	turtleRight ( turtle , 90) ;
-	levy ( turtle , length / sqrt (2) , depth - 1) ;
-	
-	turtleLeft ( turtle , 45) ;
+  turtleRight(turtle, 90);
+  levy(turtle, length / sqrt(2), depth - 1);
+
+  turtleLeft(turtle, 45);
 }
 
-int main(void){
+int main(void) {
   TurtleApp *app = turtleAppCreate(400, 200, "Test Line");
 
-  if(app == NULL)
+  if (app == NULL)
     return 1;
 
   Turtle *t = turtleAppGetTurtle(app);
 
+  // Moviendo a la tortuga a un lugar adecuado para comenzar a dibujar
   turtlePenUp(t);
-  turtleGoTo(t, 50.0f, 100.0f);
+  turtleLeft(t, 90);
+  turtleBackward(t, 50);
   turtlePenDown(t);
-
-  turtleSetColor(t, 255, 100, 0);
-  turtleSetSpeed(t, 5.0f);
-  turtleForward(t, 300.0f);
 
   turtleAppRun(app);
   turtleAppDestroy(app);
