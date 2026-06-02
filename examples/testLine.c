5e991ae43d70f9708fdaabef9924a348c7700d6a
frank <259405825+frandev342@users.noreply.github.com>
2026-06-02 09:31:29 -0500

    Probando com angulo de division de ramas 30

--- examples/testLine.c
+++ examples/testLine.c
@@ -9,7 +9,7 @@ void fractalTree(Turtle *turtle, float length, int depth) {
   turtleLeft(turtle, 30);
   fractalTree(turtle, length * 0.8, depth - 1);
 
-  turtleRight(turtle, 20);
+  turtleRight(turtle, 30);
   fractalTree(turtle, length * 0.8, depth - 1);
 
   turtleLeft(turtle, 30);
