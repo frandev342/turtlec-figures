b18b09e9efdd599d7cec74aba3116779a4c05e0e
frandev342 <259405825+frandev342@users.noreply.github.com>
2026-06-01 22:35:27 -0500

    Add the recursive functions fractalTree and levy

--- examples/testLine.c
+++ examples/testLine.c
@@ -1,5 +1,34 @@
 #include "../turtlec.h"
 
+void fractalTree ( Turtle * turtle , float length , int depth) {
+	if( depth == 0 || length < 5)
+		return ;
+	turtleForward ( turtle , length ) ;
+
+	turtleLeft ( turtle , 30) ;
+	fractalTree ( turtle , length * 0.8 , depth - 1) ;
+
+	turtleRight ( turtle , 60) ;
+	fractalTree ( turtle , length * 0.8 , depth - 1) ;
+
+	turtleLeft ( turtle , 30) ;
+	turtleBackward ( turtle , length ) ;
+}
+
+void levy ( Turtle * turtle , float length , int depth ) {
+	if( depth == 0) {
+		turtleForward ( turtle , length ) ;
+		return ;
+	}
+	turtleLeft ( turtle , 45) ;
+	levy ( turtle , length / sqrt (2) , depth - 1) ;
+
+	turtleRight ( turtle , 90) ;
+	levy ( turtle , length / sqrt (2) , depth - 1) ;
+	
+	turtleLeft ( turtle , 45) ;
+}
+
 int main(void){
   TurtleApp *app = turtleAppCreate(400, 200, "Test Line");
 
