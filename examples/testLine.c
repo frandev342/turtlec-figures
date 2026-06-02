ea816be830acf4c9b7ecd4e5af0ce697094baf55
frank <259405825+frandev342@users.noreply.github.com>
2026-06-02 10:59:56 -0500

    cambiando la coloración del arbol fractal por los valores 89, 45, 15

--- examples/testLine.c
+++ examples/testLine.c
@@ -20,9 +20,9 @@ void fractalTree(Turtle *turtle, float length, int depth) {
 
   turtleLeft(turtle, 30);
 
-  int r = (17 * depth) % 256;
-  int g = (56 * depth) % 256;
-  int b = (78 * depth) % 256;
+  int r = (89 * depth) % 256;
+  int g = (45 * depth) % 256;
+  int b = (15 * depth) % 256;
   turtleSetColor(turtle, r, g, b);
 
   turtleBackward(turtle, length);
